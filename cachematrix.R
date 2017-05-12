465ad58b506b3aa0800a6174d33c0f73dae306da
cyanophyta <dibyendughosh@gmail.com>
2017-05-12 22:57:21 +0530

    Add files via upload
    1st draft for review; includes sample tests in comments at the end

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix functon creates a vector of getters and setters
+## for a square matrix and its inverse matrix and these functions
+## saves/accessess the two matrices to/from cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invmat <- NULL
+        setmat <- function(y) {
+                  x <<- y
+                  invmat <<- NULL
+        }
+        getmat <- function() {
+                  x
+        }
+        setinv <- function(matinv) {
+                  invmat <<- matinv
+        }
+        getinv <- function() {
+                  invmat
+        }
+        list(setmat = setmat,
+             getmat = getmat,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Given a square matrix, for which makeCacheMatrix function was called
+## that returned a list of getters/setters, using those functions, 
+## this function caches inverse of the original matrix
+## unless it finds the inverse matrix already in the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return inverse of the matrix which was passed to
+        ## makeCacheMatrix function.
+        invmat <- x$getinv()
+        if(!is.null(invmat)) {
+          message("getting cached inverse of matrix")
+          return(invmat)
+        }
+        data <- x$getmat()
+        invmat <- solve(data, ...)
+        x$setinv(invmat)
+        invmat
 }
+
+#> source("cacheMatrix.R")
+#> flist <-makeCacheMatrix(matrix(1))
+#> cacheSolve(flist)
+#[,1]
+#[1,]    1
+#> cacheSolve(flist)
+#getting cached inverse of matrix
+#[,1]
+#[1,]    1
+#> flist <-makeCacheMatrix(matrix(1:4, nrow = 2, ncol = 2))
+#> cacheSolve(flist)
+#[,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+#> cacheSolve(flist)
+#getting cached inverse of matrix
+#[,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
